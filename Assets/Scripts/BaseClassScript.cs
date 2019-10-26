6de8e1e8d5fee3d3d5a0c949ddc53b828afd02da
Diak29 <Diak29@DESKTOP-PU3FR45>
2019-10-26 11:47:26 +0200

    BaseClasses and BaseWarriot

---
+++ Assets/Scripts/BaseClassScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class BaseClassScript : MonoBehaviour
+{
+    private string className;
+    private int strength, health, intelligent, damage;
+    private bool shoot;
+    public string ClassName
+    {
+        get { return className; }
+        set { className = value; }
+    }
+
+    public int Strength
+    {
+        get { return strength; }
+        set { strength = value; }
+    }
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public int Intelligent
+    {
+        get { return intelligent; }
+        set { intelligent = value; }
+    }
+
+    public int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public bool Shoot
+    {
+        get { return shoot; }
+        set { shoot = value; }
+    }
+}
---
+++ Assets/Scripts/BaseWarrior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BaseWarrior : BaseClassScript
+{
+    public BaseWarrior()
+    {
+        ClassName = "Warrior";
+        Strength = 5;
+        Health = 10;
+        Intelligent = 2;
+        Damage = Strength * 2;
+        Shoot = false;
+    }
+}
