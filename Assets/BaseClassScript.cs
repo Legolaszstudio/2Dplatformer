052f919a26f1c2919cdef94cb141162248bef903
NAdam11 <nemeth.adam.mail@gmail.com>
2019-10-26 11:54:29 +0200

    g

---
+++ Assets/BaseClassScript.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+public class BaseClassScript : MonoBehaviour
+{
+
+    private string className;
+    private int strength;
+    private int health;
+    private int intelligent;
+    private int damage;
+    private bool shoot;
+
+    public string ClassName
+    {
+        get { return className; }
+        set { className = value; }
+    }
+    public int Strength
+    {
+        get { return strength; }
+        set { strength = value; }
+    }
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+    public int Intelligent
+    {
+        get { return intelligent; }
+        set { intelligent = value; }
+    }
+    public int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+    public bool Shoot
+    {
+        get { return shoot; }
+        set { shoot = value; }
+    }
+}
---
+++ Assets/BaseWizardScript.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class BaseWizardScript : BaseClassScript
+{
+    public BaseWizardScript()
+    {
+        ClassName = "Wizard";
+        Strength = 5;
+        Health = 8;
+        Intelligent = 5;
+        Damage = Strength * 2;
+        Shoot = false;
+    }
+}
